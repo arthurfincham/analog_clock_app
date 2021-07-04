9271bbd956e715af1afcac5ace9ed10a99146b60
ARTHUR FINCHAM <arthurfincham@me.com>
2021-07-04 16:42:11 +0100

    Initial Commit

---
+++ analog_clock/analog_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  analog_clockApp.swift
+//  analog_clock
+//
+//  Created by Arthur Fincham on 04/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct analog_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
